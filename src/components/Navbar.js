9f7c03c64774b28d7ee7e69fdde274126faff3e9
Felizj17 <felizj17@gmail.com>
2023-04-19 15:14:01 -0400

    navbar style and function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-export default function Navbar(){
-    return(
-        <div>
-            
-        </div>
-    )
+import { Link, useNavigate } from 'react-router-dom'
+import './navbar.css'
+import dollarImg from '../img/dollarSign.png'
+export default function Navbar({ total }) {
+    const navigate = useNavigate()
+  return (
+    <nav>
+      <h1 className='title'>
+        <img src={dollarImg} alt='dollarSign' />
+        <Link className='home-link' to='/'>
+          Budgtr
+        </Link>
+        <img src={dollarImg} alt='dollarSign' />
+      </h1>
+
+      <div className='nav-sub'>
+        <h2>Account Total:${total.toFixed(2)}</h2>
+        <button id='new-btn' onClick={()=>{navigate('/new')}}>
+            New Transaction
+        </button>
+      </div>
+    </nav>
+  )
 }
